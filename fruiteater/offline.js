d553bcb52115bc8a85b1f23c8119e24827e28d1f
24engiball <engiball@gmail.com>
2022-01-22 16:20:43 +0700

    test
    test

---
+++ fruiteater/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1642842886,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/fruit-sheet0.png",
+		"images/bplay-sheet0.png",
+		"images/breplay-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy2-sheet0.png",
+		"media/ok - 1.m4a",
+		"media/ok - 1.ogg",
+		"media/mattoglseby - 1.m4a",
+		"media/mattoglseby - 1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
